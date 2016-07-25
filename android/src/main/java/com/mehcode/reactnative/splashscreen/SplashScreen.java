06dc8cc4ef97a9ca54c50e95dcb2855447b862f6
Ryan Leckey <leckey.ryan@gmail.com>
2016-07-24 19:59:10 -0700

    Use WeakReference and null-checking to ensure we don't crash if SplashScreen is hidden before the first render cycle

--- android/src/main/java/com/mehcode/reactnative/splashscreen/SplashScreen.java
+++ android/src/main/java/com/mehcode/reactnative/splashscreen/SplashScreen.java
@@ -3,13 +3,22 @@ package com.mehcode.reactnative.splashscreen;
 import android.app.Activity;
 import android.app.Dialog;
 
+import java.lang.ref.WeakReference;
+
 public class SplashScreen {
     private static Dialog mSplashDialog;
+    private static WeakReference<Activity> mActivity;
 
     /**
      * Show the splash screen.
      */
     public static void show(final Activity activity) {
+        if (activity == null) return;
+
+        // Store weak-reference to showing activity (in case we try to hide too early)
+        // NOTE: For instance in direct execution of index.android.js
+        mActivity = new WeakReference<Activity>(activity);
+
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -30,7 +39,10 @@ public class SplashScreen {
     /**
      * Close the active splash screen.
      */
-    public static void hide(final Activity activity) {
+    public static void hide(Activity activity) {
+        if (activity == null) activity = mActivity.get();
+        if (activity == null) return;
+
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
